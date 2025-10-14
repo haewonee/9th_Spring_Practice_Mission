f423d39e4aa00f79ad96f897d83ef92305b97e87
haewonee <godnjs5870@naver.com>
2025-10-14 20:10:35 +0900

    feat: Food 엔티티 name 컬럼에 unique 속성 추가

--- src/main/java/umc/domain/food/entity/Food.java
+++ src/main/java/umc/domain/food/entity/Food.java
@@ -16,7 +16,7 @@ public class Food extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name",nullable = false)
+    @Column(name = "name",nullable = false, unique = true)
     @Enumerated(EnumType.STRING)
     private FoodName name;
 
